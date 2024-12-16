f4f23a9de6b8973cd00874a2a4bb8dc466eb8414
roymnode <roym.node@gmail.com>
2024-12-16 15:07:03 +0200

    fix product number

--- Class4-16.12.24/first-router-app/src/components/Products/Products.tsx
+++ Class4-16.12.24/first-router-app/src/components/Products/Products.tsx
@@ -5,8 +5,8 @@ const Products = () =>{
         <div>
             This is the products
             <Link to={'/product-full-page/1'}>{'product 1'}</Link>
-            <Link to={'/product-full-page/2'}>{'product 1'}</Link>
-            <Link to={'/product-full-page/3'}>{'product 1'}</Link>
+            <Link to={'/product-full-page/2'}>{'product 2'}</Link>
+            <Link to={'/product-full-page/3'}>{'product 3'}</Link>
         </div>
     )
 }
