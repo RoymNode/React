f848045326adb7aca65d581ebda66ec451118d53
roymnode <roym.node@gmail.com>
2024-12-23 11:49:41 +0200

    class 5

---
+++ Class5-23.12.24/state-app/src/App.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Product } from './components/types';
+import ProductsGallery from './components/ProductsGallery/ProductsGallery';
+import BigCard from './components/BigCard/BigCard';
+
+function App() {
+  const [clickedProduct, setClickedProduct] = useState<Product>();
+  return (
+    <div className="App">
+      <ProductsGallery setClickedProduct={setClickedProduct}/>
+      <BigCard clickedProduct={clickedProduct} />
+    </div>
+  );
+}
+
+export default App;
---
+++ Class5-23.12.24/state-app/src/components/types.ts
@@ -0,0 +1,11 @@
+export interface Product{
+  id: number;
+  name: string;
+  isFavorite: boolean;
+}
+
+export interface City{
+  id: number;
+  name: string;
+  isFavorite: boolean;
+}
